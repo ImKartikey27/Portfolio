f49e1e4198c3f87713d37dc5fe073c7510923882
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-07-04 19:55:40 +0530

    ADD: feature cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import LogoSection from "./components/LogoSection"
 import NavBar from "./components/NavBar"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
 
@@ -11,6 +12,7 @@ const App = () => {
     <Hero/>
     <ShowcaseSection/>
     <LogoSection/>
+    <FeatureCards/>
     </>
     
   )
